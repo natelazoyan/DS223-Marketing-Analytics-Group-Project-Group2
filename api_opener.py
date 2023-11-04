77394d8662a2cc336f2fe32cd79c18139a0bc63b
anahit1baghdasarian <andemetra1@gmail.com>
2023-11-04 20:21:39 +0400

    importing main.py from CLV_Analysis/API

--- api_opener.py
+++ api_opener.py
@@ -1,9 +1,10 @@
 import subprocess
 import webbrowser
+from CLV_Analysis.API import main 
 
 def start_fastapi():
-    subprocess.run(["uvicorn", "main:app", "--reload"])
-    webbrowser.open('http://127.0.0.1:8000/docs')
+    subprocess.run(["uvicorn", "CLV_Analysis.API.main:app", "--reload"])
+    webbrowser.open('http://127.0.0.1:8000/docs#/')
 
 name = "__main__"
 
